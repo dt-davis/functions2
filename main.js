6b8ca9248bd65c3af8e39f7e040b9b027c22dbad
Donovan Davis <dt-davis@Hotmail.com>
2020-05-24 20:16:45 -0500

    added working power function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function add (x, y){
     return x + y
 }
 
-console.log(add(2,4))
+console.log('add function:   ' + add(2,4))
 
 function multiply ( x, y){
     let total = 0
@@ -12,17 +12,16 @@ function multiply ( x, y){
     return total
 }
 
-console.log(multiply(6,4))
-console.log(multiply(2,8))
+console.log('multiply function:   ' + multiply(6,4))
+console.log('multiply function:   ' + multiply(2,8))
 
 function power(x,y){
-    let total = 1
-    for (let counter = 0; counter <= y; counter++){
-        total += total * (multiply(x,y))
-        console.log(total)
+    let total = multiply(x,x)
+    for(let counter = 3; counter <=y; counter++){
+        total = multiply(total,x)
     }
     return total
 }
 
-console.log(power(2,8))
-console.log(power(3,4))
+console.log('power function:   ' + power(2,8))
+console.log('power function:   ' + power(3,4))
